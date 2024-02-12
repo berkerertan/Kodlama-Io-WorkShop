a1218333ee02c54f7f2e5d0f6e83359b98b4c662
Berker ERTAN <berkerertan@hotmail.com>
2024-02-12 18:21:19 +0300

    Güncel

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿using Kodlama_Io_WorkShop.Entities;
+﻿using Kodlama_Io_WorkShop.Business.Concretes;
+using Kodlama_Io_WorkShop.Business.Dtos.Requests;
+using Kodlama_Io_WorkShop.Business.Dtos.Responses;
+using Kodlama_Io_WorkShop.DataAccess.Concretes.InMemory;
+using Kodlama_Io_WorkShop.Entities;
 using System.Globalization;
 
 namespace Kodlama_Io_WorkShop
@@ -7,7 +11,28 @@ namespace Kodlama_Io_WorkShop
     {
         static void Main(string[] args)
         {
-             
+
+
+            TrainerManager trainerManager = new TrainerManager(new ImTrainerDal());
+            foreach (var category in trainerManager.GetTrainers())
+            {
+                Console.WriteLine(category.TrainerName);
+                Console.WriteLine(category.TrainerId);
+            }
+            Console.WriteLine("-------------------");
+            trainerManager.Delete(1);
+
+            trainerManager.Update(2,new UpdateTrainerRequest { TrainerName="sdasdasd"});
+
+            foreach (var category in trainerManager.GetTrainers())
+            {
+                Console.WriteLine(category.TrainerName);
+            }
+
+
+
+
+
         }
     }
 }
